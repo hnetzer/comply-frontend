3374736920373398f8659b7d2d97ada7344398ef
Henry Netzer <hnetzer19@gmail.com>
2020-01-15 13:10:28 -0800

    made a home page with login or signup buttons

--- src/App.js
+++ src/App.js
@@ -3,12 +3,14 @@ import { Router } from "@reach/router"
 
 import FilingScheduleScreen from "./screens/FilingScheduleScreen"
 import GetStartedScreen from "./screens/GetStartedScreen"
+import HomeScreen from "./screens/HomeScreen"
 
 const App = (props) => {
 
   return (
     <Router>
-      <GetStartedScreen path="/" />
+      <HomeScreen path="/" />
+      <GetStartedScreen path="/get-started" />
       <FilingScheduleScreen path="/schedule" />
     </Router>
   )
