7d15427af6b97964ea150f79f88c9c36180da351
Henry Netzer <hnetzer19@gmail.com>
2020-04-11 13:19:46 -0700

    added yup validation with formik and bootstrap formcontrol feedback

--- src/forms/adminFilingForm.js
+++ src/forms/adminFilingForm.js
@@ -1,26 +1,59 @@
 import React, { useState } from 'react';
-
 import { Formik, FieldArray } from 'formik';
-import Card from 'react-bootstrap/Card';
-import Button from 'react-bootstrap/Button';
-
-import style from './adminFilingForm.module.css'
-
-import AdminFilingDueDateSection from './adminFilingDueDateSection'
+import * as Yup from 'yup';
 
 // Bootstrap components
 import Form from 'react-bootstrap/Form';
+import FormControl from 'react-bootstrap/FormControl';
+import Card from 'react-bootstrap/Card';
+import Button from 'react-bootstrap/Button';
+
+import AdminFilingDueDateSection from './adminFilingDueDateSection'
+
+import style from './adminFilingForm.module.css'
+
+const FilingSchema = Yup.object().shape({
+  name: Yup.string().min(3, 'Too short!').required('Required'),
+  agency_id: Yup.number().integer().required('Required'),
+  agency: Yup.object().shape({
+    jurisdiction_id: Yup.number().integer().required('Required')
+  }),
+  fields: Yup.array().of(Yup.object().shape({
+    name: Yup.string().required('Required'),
+    helper_text: Yup.string().nullable(),
+    order: Yup.number().integer('Number must be an integer').nullable()
+  })),
+  occurence: Yup.mixed().oneOf(['annual', 'multiple', 'biennial']),
+  due_dates: Yup.array().of(Yup.object().shape({
+    fixed_month: Yup.number().integer().nullable(),
+    fixed_day: Yup.number().integer().nullable(),
+    month_offset: Yup.number().integer().nullable(),
+    day_offset: Yup.mixed().oneOf(['1', '15', 'end-of-month', null]),
+    offset_type: Yup.mixed().oneOf(['none', 'registration', 'year-end']),
+  }))
+});
+
+const FilingInitialValues = {
+  name: '',
+  agency_id: '',
+  agency: { jurisdiction_id: '' },
+  fields: [],
+  occurrence: 'annual',
+  due_dates: [{
+    fixed_month: null,
+    fixed_day: null,
+    month_offset: null,
+    day_offset: null,
+    offset_type: 'none'
+  }]
+};
+
 
 const AdminFilingForm = ({ filing, jurisdictions, agencies, handleSubmit, status }) => {
   const [validated] = useState(false);
 
   const submit = async (values, { setSubmitting }) => {
     await handleSubmit(values, { setSubmitting })
-  }
-
-  const validate = values => {
-    const errors = {};
-    return errors;
   }
 
   const renderAgencies = (values) => {
@@ -35,34 +68,20 @@ const AdminFilingForm = ({ filing, jurisdictions, agencies, handleSubmit, status
   const addFormField = (arrayHelpers) => {
     arrayHelpers.push({
       name: '',
-      helper_text: null,
+      helper_text: '',
       type: 'text',
       order: null
     })
   }
 
-  const initial = filing != null ? filing :
-    {
-      name: '',
-      agency_id: '',
-      agency: { jurisdiction_id: '' },
-      fields: [],
-      occurrence: 'annual',
-      due_dates: [{
-        fixed_month: null,
-        fixed_day: null,
-        month_offset: null,
-        day_offset: null,
-        offset_type: 'none'
-      }]
-    };
-
   return (
     <div className={style.container}>
       <Formik
-        initialValues={initial}
-        validate={validate}
+        initialValues={filing != null ? filing : FilingInitialValues}
         onSubmit={submit}
+        validationSchema={FilingSchema}
+        validateOnBlur={false}
+        validateOnChange={false}
         enableReinitialize={true}
       >
       {({
@@ -76,17 +95,19 @@ const AdminFilingForm = ({ filing, jurisdictions, agencies, handleSubmit, status
         /* and other goodies */
       }) => (
           <Form validated={validated} onSubmit={handleSubmit} className={style.form}>
+            {console.log(errors)}
             <Form.Group controlId="name">
               <Form.Label>Filing Name</Form.Label>
               <Form.Control
-                required
                 onChange={handleChange}
                 type="text"
                 size="lg"
+                isInvalid={errors.name}
                 autoComplete="off"
                 style={{ width: 360 }}
                 placeholder=""
                 value={values.name} />
+              <FormControl.Feedback type='invalid'>{errors.name}</FormControl.Feedback>
             </Form.Group>
             <div className={style.cardRow}>
               <Card className={style.shortCard}>
@@ -95,9 +116,9 @@ const AdminFilingForm = ({ filing, jurisdictions, agencies, handleSubmit, status
                   <Form.Group controlId="agency.jurisdiction_id">
                     <Form.Label>Jurisdiction</Form.Label>
                     <Form.Control
-                      required
                       onChange={handleChange}
                       value={values.agency.jurisdiction_id}
+                      isInvalid={errors.agency && errors.agency.jurisdiction_id}
                       as="select">
                       <option value=""></option>
                       {jurisdictions.map((j,i) =>
@@ -107,9 +128,9 @@ const AdminFilingForm = ({ filing, jurisdictions, agencies, handleSubmit, status
                   <Form.Group controlId="agency_id">
                     <Form.Label>Agency</Form.Label>
                     <Form.Control
-                      required
                       onChange={handleChange}
                       value={values.agency_id}
+                      isInvalid={errors.agency_id}
                       as="select">
                       <option value=""></option>
                       {renderAgencies(values)}
@@ -143,10 +164,10 @@ const AdminFilingForm = ({ filing, jurisdictions, agencies, handleSubmit, status
                       <Form.Row key={index}>
                         <Form.Group controlId={`fields[${index}].name`}>
                           <Form.Control
-                            required
                             onChange={handleChange}
                             type="text"
                             placeholder="Field name"
+                            isInvalid={errors.fields && errors.fields[index] && errors.fields[index].name}
                             style={{ width: 256, marginRight: 16 }}
                             autoComplete="off"
                             value={values.fields[index].name} />
@@ -166,7 +187,6 @@ const AdminFilingForm = ({ filing, jurisdictions, agencies, handleSubmit, status
                             type="number"
                             style={{ width: 56, paddingRight: 8 }}
                             autoComplete="off"
-                            placeholder={index+1}
                             value={values.fields[index].order || ''} />
                         </Form.Group>
                       </Form.Row>
@@ -181,7 +201,7 @@ const AdminFilingForm = ({ filing, jurisdictions, agencies, handleSubmit, status
                 <div style={{ color: 'green', marginRight: 16 }}>{status}</div>
               ) : null}
               <Button className={style.submitButton} variant="secondary" type="submit">
-                { initial.id == null ? 'Create Filing' : 'Update Filing'}
+                { values.id == null ? 'Create Filing' : 'Update Filing'}
               </Button>
             </div>
           </Form>
