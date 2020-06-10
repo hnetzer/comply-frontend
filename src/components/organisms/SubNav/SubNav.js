83389fdf95d72e73bfecead7d37e115888c0c76e
Henry Netzer <hnetzer19@gmail.com>
2020-06-10 15:53:57 -0700

    highlight correct tab with subnav

--- src/components/organisms/SubNav/SubNav.js
+++ src/components/organisms/SubNav/SubNav.js
@@ -3,19 +3,27 @@ import { Link } from '@reach/router'
 import styles from './SubNav.module.scss';
 
 const SubNav = ({ location }) => {
-  console.log('subnav', location)
+  const pathname = window.location.pathname
+
+  const getClassName = (path) => {
+    if (path === pathname) {
+      return styles.navLinkSelected
+    }
+    return styles.navLink
+  }
+
   return (
     <div className={styles.subNav}>
-      <Link className={styles.navLink} to="/home">
+      <Link className={getClassName("/home")} to="/home">
         Dashboard
       </Link>
-      <Link className={styles.navLink} to="/home/company">
+      <Link className={getClassName("/home/company")} to="/home/company">
         Company
       </Link>
-      <Link className={styles.navLink} to="/home/guide">
+      <Link className={getClassName("/home/guide")} to="/home/guide">
         Guide to Filing
       </Link>
-      <Link className={styles.navLink} to="/home/faqs">
+      <Link className={getClassName("/home/faqs")} to="/home/faqs">
         FAQs
       </Link>
     </div>
