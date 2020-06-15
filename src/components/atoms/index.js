d8b9d15457cc617ab21a91feb2eea27608b7d36d
Henry Netzer <hnetzer19@gmail.com>
2020-06-15 10:05:04 -0700

    removing A LOT of unneccesary files

--- src/components/atoms/index.js
+++ src/components/atoms/index.js
@@ -1,18 +1,8 @@
-import FilingName from './FilingName/FilingName';
-import FilingJurisdiction from './FilingJurisdiction/FilingJurisdiction'
-import FilingAgency from './FilingAgency/FilingAgency'
-import FilingStatus from './FilingStatus/FilingStatus'
-import FilingDueDate from './FilingDueDate/FilingDueDate'
 import Card from './Card/Card'
 import Alert from './Alert/Alert'
 import { Table, Body, Header, HeaderCell, Row, Cell } from './Table/Table';
 
 export {
-  FilingName,
-  FilingJurisdiction,
-  FilingAgency,
-  FilingStatus,
-  FilingDueDate,
   Card,
   Table,
   Body,
--- src/components/molecules/index.js
+++ src/components/molecules/index.js
@@ -1,26 +1,13 @@
 import AccountMenu from './AccountMenu/AccountMenu';
-import FilingCard from './FilingCard/FilingCard';
 import InfoPopover from './InfoPopover/InfoPopover';
-import FilingHeader from './FilingHeader/FilingHeader';
-import FilingDataList from './FilingDataList/FilingDataList';
-import FilingAlertMessage from './FilingAlertMessage/FilingAlertMessage';
 import FilingTimeline from './FilingTimeline/FilingTimeline';
-import DatePicker from './DatePicker';
 import VerticalProgressBar from './VerticalProgressBar/VerticalProgressBar';
 import QuestionToggle from './QuestionToggle/QuestionToggle';
 
-// Admin
-import SideListItem from './SideListItem'
 
 export {
   AccountMenu,
-  FilingCard,
   InfoPopover,
-  FilingHeader,
-  FilingDataList,
-  FilingAlertMessage,
-  SideListItem,
-  DatePicker,
   FilingTimeline,
   VerticalProgressBar,
   QuestionToggle
