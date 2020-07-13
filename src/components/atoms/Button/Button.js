8646805b6fedf84a2fe48995ccf8978a2e83996e
Henry Netzer <hnetzer19@gmail.com>
2020-07-13 16:17:31 -0700

    iterated on the button component and feedback card

--- src/components/atoms/Button/Button.js
+++ src/components/atoms/Button/Button.js
@@ -2,15 +2,40 @@ import React from 'react';
 
 import style from './Button.module.scss';
 
-const Button = ({ children }) => {
+const Button = ({ children, onClick, type, variant, outline, disabled }) => {
+  const getButtonStyle = () => {
+    if (disabled) {
+      return style.disabled;
+    }
+
+    if (!outline) {
+      switch(variant) {
+        case "primary": return style.primary;
+        case "secondary": return style.secondary;
+        default: return style.primary;
+      }
+    } else {
+      switch(variant) {
+        case "primary": return style.primaryOutline;
+        case "secondary": return style.secondaryOutline;
+        default: return style.primaryOutline;
+      }
+    }
+  }
+
+
+
   return (
-    <button className={style.primaryOutline}>
+    <button type={type} onClick={onClick} className={getButtonStyle()}>
       {children}
     </button>
   )
 }
 
 Button.defaultProps = {
+  variant: "primary",
+  outline: false,
+  disabled: false,
   // show: true,
 }
 
