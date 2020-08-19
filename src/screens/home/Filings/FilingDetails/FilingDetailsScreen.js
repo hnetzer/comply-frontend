810db10a22583f70c09584357bfbbcd70eddc963
Henry Netzer <hnetzer19@gmail.com>
2020-08-19 15:16:58 -0700

    filled out the filing details screen

--- src/screens/home/Filings/FilingDetails/FilingDetailsScreen.js
+++ src/screens/home/Filings/FilingDetails/FilingDetailsScreen.js
@@ -2,21 +2,76 @@ import React from 'react';
 import { connect } from 'react-redux';
 import { navigate } from "@reach/router"
 
+import { getFiling } from 'network/api'
+
 import style from './FilingDetailsScreen.module.scss'
 import { Card, Button } from 'components/atoms'
 
+import { faFileAlt, faMapMarkerAlt, faLandmark, faArrowRight, faArrowLeft } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+
 class FilingDetailsScreen extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = { filing: null }
+  }
+
+  async componentDidMount() {
+    const filing = await getFiling(this.props.filingId);
+    this.setState({ filing: filing })
+  }
+
   goToAllFilings = () => {
     navigate('/home/filings')
   }
 
+
+
+
   render() {
+    const { filing } = this.state
+    if (!filing) return 'loading...'
     return (
       <section className={style.container}>
-        <Button onClick={this.goToAllFilings} variant="secondary">Back to all filings</Button>
-        <Card style={{ width: '100%', height: 100, flexDirection: 'row', marginTop: 16 }}>
-          <h2>Filing Name</h2>
-        </Card>
+        <Button onClick={this.goToAllFilings} variant="secondary">
+          <FontAwesomeIcon style={{ height: 16, width: 16, marginRight: 8 }} icon={faArrowLeft} />
+          <span>Back to all filings</span>
+        </Button>
+        {!filing ? 'loading...' : (
+          <>
+          <Card className={style.titleCard}>
+            <div>
+              <FontAwesomeIcon style={{ height: 32, width: 24 }} icon={faFileAlt} />
+              <span className={style.title}>{filing.name}</span>
+            </div>
+            <div>
+              <div>
+                <FontAwesomeIcon style={{ height: 16, width: 16 }} icon={faMapMarkerAlt} />
+                <span className={style.jurisdiction}>{filing.agency.jurisdiction.name}</span>
+              </div>
+              <div>
+                <FontAwesomeIcon style={{ height: 16, width: 16 }} icon={faLandmark} />
+                <span className={style.agency}>{filing.agency.name}</span>
+              </div>
+            </div>
+          </Card>
+          <Card className={style.descriptionCard}>
+            <h4 className={style.descriptionTitle}>Description</h4>
+            <p className={style.descriptionText} style={{ }}>
+              {filing.description}
+            </p>
+            <div className={style.descriptionFooter}>
+            {filing.website != null ?
+              (<Button onClick={() => window.open(filing.website)} variant="secondary" outline>
+                <span>Filing Website</span>
+                <FontAwesomeIcon style={{ height: 16, width: 16, marginLeft: 8 }} icon={faArrowRight} />
+               </Button>)
+              : null}
+            </div>
+          </Card>
+          </>
+
+        )}
       </section>
     )
   }
