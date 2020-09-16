4257dc4d24330f7720532e39932a1a58aa300eb3
Henry Netzer <hnetzer19@gmail.com>
2020-09-16 13:10:20 -0700

    redirect back to login page when accessing an application page after not being logged in

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
-import { Router } from "@reach/router"
+import { connect } from 'react-redux';
+import { Router, Redirect } from "@reach/router"
 
 // Signup
 import SignupScreen from 'screens/signup/SignupScreen'
@@ -15,18 +16,44 @@ import LoginScreen from 'screens/public/Login/LoginScreen'
 import AdminScreen from "./screens/admin/AdminScreen"
 
 
-const App = (props) => {
+const App = ({ auth }) => {
+
+  const isAuth = (auth.user) != null && (auth.token != null)
+
+  const renderProtectedRoutes = () => {
+    return (
+      <>
+        <OnboardingScreen path="/onboarding/*" />
+        <AdminScreen path="/admin/*" />
+        <HomeScreen path="/home/*" />
+      </>
+    )
+  }
+
+  const renderRedirects = () => {
+    return (
+      <>
+        <Redirect from="/onboarding/*" to="/login" noThrow />
+        <Redirect from="/admin/*" to="/login" noThrow />
+        <Redirect from="/home/*" to="/login" noThrow />
+      </>
+    )
+  }
 
   return (
     <Router className="rootRouter">
       <WebsiteScreen path="/" />
       <LoginScreen path="/login" />
-      <HomeScreen path="/home/*" />
       <SignupScreen path="/signup" />
-      <OnboardingScreen path="/onboarding/*" />
-      <AdminScreen path="/admin/*" />
+      {isAuth ? renderProtectedRoutes() : renderRedirects()}
     </Router>
   )
 }
 
-export default App;
+const mapStateToProps = state => {
+  return {
+    auth: state.auth
+  }
+}
+
+export default connect(mapStateToProps)(App);
